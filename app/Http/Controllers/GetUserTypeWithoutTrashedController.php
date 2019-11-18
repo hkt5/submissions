b325d0aa4ffec02ec043194ebd187258c30039a0
Artur Mostowiak <56314571+arturpuck@users.noreply.github.com>
2019-11-18 09:43:29 +0100

    Update GetUserTypeWithoutTrashedController.php

--- app/Http/Controllers/GetUserTypeWithoutTrashedController.php
+++ app/Http/Controllers/GetUserTypeWithoutTrashedController.php
@@ -25,7 +25,6 @@ class GetUserTypeWithoutTrashedController extends Controller
      * [Returns user type data by given id but returns only users that are not soft deleted.]
      *
      * @queryParam id required id of a user type
-
      * @response 200 {"content":{"user_type":{"id":7,"name":"test user type","deleted_at":null,"created_at":"2019-11-15 13:55:45","updated_at":"2019-11-15 13:55:45"}},"error_messages":[]}
      * @response 400 {"content":[],"error_messages":{"id":["The selected id is invalid."]}}
      * @response 400 {"content":[],"error_messages":{"id":["The id must be an integer."]}}
