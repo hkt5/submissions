b64320397544b943402c26a2e083b1d87474447c
Artur Mostowiak <56314571+arturpuck@users.noreply.github.com>
2019-11-18 09:39:19 +0100

    Update CreateUserTypeController.php

--- app/Http/Controllers/CreateUserTypeController.php
+++ app/Http/Controllers/CreateUserTypeController.php
@@ -23,11 +23,8 @@ class CreateUserTypeController extends Controller
     /**
      * Create new user type.
      * [ Create new user type. ]
-     *
      * @bodyParam name string required  Name of a user type. Must contain between 3 - 167 characters and must be unique. Two or more exacly the same names are not allowed.
-     
-     * @response 200 
-       {"content":{"user_type":{"name":"some name","updated_at":"2019-11-15 12:39:00","created_at":"2019-11-15 12:39:00","id":3}},"error_messages":[]}
+     * @response 200 {"content":{"user_type":{"name":"some name","updated_at":"2019-11-15 12:39:00","created_at":"2019-11-15 12:39:00","id":3}},"error_messages":[]}
      * @response 400 {"content":[],"error_messages":{"name":["The name field is required."]}}
      * @response 400 {"content":[],"error_messages":{"name":["The name has already been taken."]}}
      * @response 400 {"content":[],"error_messages":{"name":["The name must be at least 3 characters."]}}
