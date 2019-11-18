260a149c47954d9e01b9ff335d4059ecdd1eeca8
Artur Mostowiak <56314571+arturpuck@users.noreply.github.com>
2019-11-18 09:48:18 +0100

    Update UpdateUserTypeController.php

--- app/Http/Controllers/UpdateUserTypeController.php
+++ app/Http/Controllers/UpdateUserTypeController.php
@@ -24,9 +24,8 @@ class UpdateUserTypeController extends Controller
      *
      * @bodyParam id integer required Id of a user type you want to update.
      * @bodyParam name string required User type name you want to update. Must contain 3-167 characters.
-     
-     * @response 200 
-     {"content":{"user_type":{"id":7,"name":"testing","deleted_at":null,"created_at":"2019-11-15 13:55:45","updated_at":"2019-11-15 14:59:29"}},"error_messages":[]}
+     *
+     * @response 200 {"content":{"user_type":{"id":7,"name":"testing","deleted_at":null,"created_at":"2019-11-15 13:55:45","updated_at":"2019-11-15 14:59:29"}},"error_messages":[]}
      * @response 400 {"content":[],"error_messages":{"id":["The selected id is invalid."]}}
      * @response 400 {"content":[],"error_messages":{"id":["The id must be an integer."]}}
      * @response 400 {"content":[],"error_messages":{"name":["The name field is required."]}}
