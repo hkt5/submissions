b28202b609cfabd26ae4a9304a2125f18d734108
Artur Mostowiak <56314571+arturpuck@users.noreply.github.com>
2019-11-18 09:40:14 +0100

    Update DeleteHardUserTypeController.php

--- app/Http/Controllers/DeleteHardUserTypeController.php
+++ app/Http/Controllers/DeleteHardUserTypeController.php
@@ -25,7 +25,6 @@ class DeleteHardUserTypeController extends Controller
      * [ To hard delete a user type means it is impossible to restore it. A row is physically deleted from the database]
      *
      * @bodyParam id integer required An id of a user type
-     
      * @response 200 {"content":{"user_type":{"id":6,"name":"some user type","deleted_at":null,"created_at":"2019-11-15 12:54:29","updated_at":"2019-11-15 12:54:29"}},"error_messages":[]}
      * @response 400 {"content":[],"error_messages":{"id":["The selected id is invalid."]}}
      * @response 400 {"content":[],"error_messages":{"id":["The id must be an integer."]}}
