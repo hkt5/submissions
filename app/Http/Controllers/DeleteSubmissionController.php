5c0e7729626d3ca4e55bc052b1a966138251a08d
Artur Mostowiak <56314571+arturpuck@users.noreply.github.com>
2019-11-18 09:41:29 +0100

    Update DeleteSubmissionController.php

--- app/Http/Controllers/DeleteSubmissionController.php
+++ app/Http/Controllers/DeleteSubmissionController.php
@@ -24,7 +24,6 @@ class DeleteSubmissionController extends Controller
      * [Deletes submission with given id]
      *
      * @bodyParam id integer required An id of a submission
-     
      * @response 200 {"content":{"submission":{"id":1,"name":"Czesiu","surname":"Kowalski","phone":"0700880788","email":"czesiu@gmail.com","developer_type":8,"developer_skill":7,"linked_in_profile":"linked_in_profile","github_profile":"http:\/\/www.github.com\/czesiukowalski","description":"awesome developer","files":"{\"file\": \"somefile.txt\"}","submission_type":1,"created_at":"2019-11-15 12:09:10","updated_at":"2019-11-15 12:09:10"}},"error_messages":[]}
      * @response 400 {"content":[],"error_messages":{"id":["The selected id is invalid."]}}
      * @response 400 {"content":[],"error_messages":{"id":["The id must be an integer."]}}
