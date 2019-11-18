7bf74bc32827a7eb412bdcfd5585842ee0e3b430
Artur Mostowiak <56314571+arturpuck@users.noreply.github.com>
2019-11-18 09:40:51 +0100

    Update DeleteSoftUserTypeController.php

--- app/Http/Controllers/DeleteSoftUserTypeController.php
+++ app/Http/Controllers/DeleteSoftUserTypeController.php
@@ -25,7 +25,6 @@ class DeleteSoftUserTypeController extends Controller
      * [To soft delete a user type means it won't be visible for query GET /user-type/without-trashed, however it is still visible for query GET /user-type/with-trashed/{id}. It also means it can be restored and still phisically exists in the database]
      *
      * @bodyParam id integer required An id of a user type
-     
      * @response 200 {"content":{"user_type":{"id":4,"name":"some user type","deleted_at":"2019-11-15 13:10:33","created_at":"2019-11-15 12:41:36","updated_at":"2019-11-15 13:10:33"}},"error_messages":[]}
      * @response 400 {"content":[],"error_messages":{"id":["The selected id is invalid."]}}
      * @response 400 {"content":[],"error_messages":{"id":["The id must be an integer."]}}
